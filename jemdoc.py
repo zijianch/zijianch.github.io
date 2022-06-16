5907946b075e739aa6d2fc1a65b5549a59cd0b1e
Zijian Chen <49643289+CharlesZJCHEN@users.noreply.github.com>
2022-06-16 12:52:48 -0500

    delete footnote

--- jemdoc.py
+++ jemdoc.py
@@ -252,7 +252,7 @@ def standardconf():
   </div>
   
   [lastupdated]
-  Page generated |, by <a href="http://jemdoc.jaboc.net/">jemdoc</a>.
+  Page generated |.
 
   [sourcelink]
   (<a href="|">source</a>)
