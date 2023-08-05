012676fdffe2764da1e57ce41437693bc8885952
Zijian Chen <charleschenzj@outlook.com>
2023-08-05 09:40:13 -0400

    update menu

--- updateMENU.sh
+++ updateMENU.sh
@@ -10,7 +10,6 @@ cd /Users/zijianchen/Desktop/personal/website
 python jemdoc3.py MENU
 
 python jemdoc3.py -c mysite.conf index
-python jemdoc3.py -c mysite.conf useful
 python jemdoc3.py -c mysite.conf jem
 python jemdoc3.py -c mysite.conf new
 python jemdoc3.py -c mysite.conf awards
@@ -20,4 +19,5 @@ python jemdoc3.py -c mysite.conf public
 python jemdoc3.py -c mysite.conf teaching
 python jemdoc3.py -c mysite.conf mentoring
 python jemdoc3.py -c mysite.conf software
-
+python jemdoc3.py -c mysite.conf resourceAcademic
+python jemdoc3.py -c mysite.conf resourceTool
