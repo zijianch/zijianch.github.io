2aa114b3090816145ed929dece1eecb5d44eee8a
Zijian Chen <charleschenzj@outlook.com>
2023-08-05 00:09:13 -0400

    add "updateMENU.sh"

---
+++ updateMENU.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is a shell script to execute Python scripts one by one.
+
+# Change the directory to your desired location
+cd /Users/zijianchen/Desktop/personal/website
+
+# Execute the Python scripts one by one
+
+python jemdoc3.py MENU
+
+python jemdoc3.py -c mysite.conf index
+python jemdoc3.py -c mysite.conf useful
+python jemdoc3.py -c mysite.conf jem
+python jemdoc3.py -c mysite.conf new
+python jemdoc3.py -c mysite.conf awards
+python jemdoc3.py -c mysite.conf topic
+python jemdoc3.py -c mysite.conf talk
+python jemdoc3.py -c mysite.conf public
+python jemdoc3.py -c mysite.conf teaching
+python jemdoc3.py -c mysite.conf software
+
