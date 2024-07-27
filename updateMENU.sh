020ad5bd372e525f84b93814e008c485c95577c6
Zijian Chen <charleschenzj@outlook.com>
2024-07-27 08:33:52 -0400

    delete useless stuffs

--- updateMENU.sh
+++ updateMENU.sh
@@ -10,14 +10,14 @@ cd /Users/zijianchen/Desktop/personal/website
 python jemdoc3.py MENU
 
 python jemdoc3.py -c mysite.conf index
-python jemdoc3.py -c mysite.conf jem
-python jemdoc3.py -c mysite.conf new
-python jemdoc3.py -c mysite.conf awards
-python jemdoc3.py -c mysite.conf topic
+#python jemdoc3.py -c mysite.conf jem
+#python jemdoc3.py -c mysite.conf new
+#python jemdoc3.py -c mysite.conf awards
+#python jemdoc3.py -c mysite.conf topic
 python jemdoc3.py -c mysite.conf talk
 python jemdoc3.py -c mysite.conf public
 python jemdoc3.py -c mysite.conf teaching
-python jemdoc3.py -c mysite.conf mentoring
+#python jemdoc3.py -c mysite.conf mentoring
 python jemdoc3.py -c mysite.conf software
-python jemdoc3.py -c mysite.conf resourceAcademic
-python jemdoc3.py -c mysite.conf resourceTool
+#python jemdoc3.py -c mysite.conf resourceAcademic
+#python jemdoc3.py -c mysite.conf resourceTool
